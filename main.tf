3970a45d392b73fb90e21dc13fa3407e2a082682
accawsdevops <acc.aws.devops@gmail.com>
2025-04-24 13:58:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1"  # Change to your preferred region
 }
 
-resource "aws_iam_user" "test_user" {
+resource "aws_iam_user" "test_user1" {
   name = "test-user1"
 }
-resource "aws_iam_user" "test_user" {
+resource "aws_iam_user" "test_user2" {
   name = "test-user2"
 }
