b2ee87210be7252d774aafba5beebbeaf3a48f57
accawsdevops <acc.aws.devops@gmail.com>
2025-05-09 18:29:41 +0530

    cloud-custodian

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"  # Change to your preferred region
 }
 
-resource "aws_iam_user" "test_user" {
-  name = "test-user"
-}
+# resource "aws_iam_user" "test_user" {
+#   name = "test-user"
+# }
